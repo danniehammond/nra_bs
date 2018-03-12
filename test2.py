3435043e012d87a30e55b1ce881c5768a52d7947
danniehammond <niioto@Lively.local>
2018-03-12 07:59:39 +0000

    New param values - Add Comments

--- test2.py
+++ test2.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Mon Mar 12 01:21:00 2018
+
+@author: niioto
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Fri Mar  9 11:48:13 2018
 
 @author: danielhammond
@@ -16,11 +24,11 @@ r=0.001  #Risk Free Rate
 t=1 #Time to expiry in years
 c =2.7240   #Call Option Price according to Black-Scholes Model
 sigma = 0.10 #Initial Volatility
-vList = []    #Array to list of volatilities
+sig = []    #Array to list of volatilities
 for i in range(0,11):
-    vList.append(i)
+    sig.append(i)
 #print(sig)  #Initial list of volatilities
-vList[0]=sigma
+sig[0]=sigma
 fList=[]
 #Newton-Raphson's ALgorithm Implementation
 for i in range(1,101):
@@ -37,12 +45,11 @@ for i in range(1,101):
     f1 = s0*ss.norm.pdf(d1)*d11 - E * m.exp(-r*t)*ss.norm.pdf(d2)*d22
     #update sigma
     sigma = sigma - f/f1
-    vList[i]= sigma
-    if(abs(vList[i]-vList[i-1])<0.00000001):
-        vList = vList[0:i+1] # Final list of volatilities
+    sig[i]= sigma
+    if(abs(sig[i]-sig[i-1])<0.00000001):
+        sig = sig[0:i+1] # Final list of volatilities
         break
-print(vList)
-#print(fList) 
+print(sig)
 #output
 #[0.1, 0.19983863364782378, 0.2000026167726357, 0.20000261744285897]
-#The implied volatility (sigma) = 0.2
+#The implied volatility siigma = 0.2
